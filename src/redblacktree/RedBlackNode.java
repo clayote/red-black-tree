5c2b6ea62793078f07e219e6856b396ce5b92dde
LogicalDash <LogicalDash@gmail.com>
2012-12-06 12:40:53 -0500

    yup

--- src/redblacktree/RedBlackNode.java
+++ src/redblacktree/RedBlackNode.java
@@ -4,21 +4,29 @@
  */
 package redblacktree;
 
+import redblacktree.RedBlackSentinel;
+
 /**
  *
  * @author sanotehu
  */
 public class RedBlackNode extends Node {
 
-    private Boolean red;
-    private int numChildren;
+    public boolean red;
+    private int numChildren, key;
     private RedBlackNode[] children;
+    private RedBlackTree tree;
 
-    RedBlackNode(int key, Boolean red) {
+    RedBlackNode(int key, boolean color, RedBlackTree tree) {
         super(key);
-        this.red = red;
+        this.red = color;
         this.children = new RedBlackNode[MAX_CHILDREN];
         this.numChildren = 0;
+        for (int i = 0; i < MAX_CHILDREN; i++) {
+            this.children[i] = new RedBlackSentinel(0, false, tree);
+        }
+        this.key = key;
+        this.tree = tree;
         this.setLevel(-1);
     }
 
@@ -44,16 +52,16 @@ public class RedBlackNode extends Node {
     }
 
     public int black_in_path(RedBlackNode node) {
-	Queue path = this.path(node);
-	RedBlackNode check = path.front();
-	int bct = 0;
-	while(check != null) {
-	    if(!check.red) {
-		bct++;
-	    }
-	    check = path.front();
-	}
-	return bct;
+        RedBlackQueue path = this.path(node);
+        RedBlackNode check = path.dequeue();
+        int bct = 0;
+        while (check != null) {
+            if (!check.red) {
+                bct++;
+            }
+            check = path.dequeue();
+        }
+        return bct;
     }
 
     @Override
@@ -65,7 +73,7 @@ public class RedBlackNode extends Node {
         }
     }
 
-    public Boolean member(RedBlackNode node) {
+    public boolean member(RedBlackNode node) {
         if (this == node) {
             return true;
         }
@@ -76,35 +84,42 @@ public class RedBlackNode extends Node {
         }
         return false;
     }
-    
+
     public int cmp(RedBlackNode that) {
-        if(this.key() > that.key()) return 1;
-        if(this.key() == that.key()) return 0;
+        if (this.key > that.key) {
+            return 1;
+        }
+        if (this.key == that.key) {
+            return 0;
+        }
         return -1;
     }
-    
-    public Boolean gt(RedBlackNode that) {
+
+    public boolean gt(RedBlackNode that) {
         return this.cmp(that) == 1;
     }
-    
-    public Boolean lt(RedBlackNode that) {
+
+    public boolean lt(RedBlackNode that) {
         return this.cmp(that) == -1;
     }
-    
-    public Boolean eq(RedBlackNode that) {
+
+    public boolean eq(RedBlackNode that) {
         return this.cmp(that) == 0;
     }
-    
-    public Boolean ge(RedBlackNode that) {
-        if(that == null) return true;
-        else return this.cmp(that) >= 1;
+
+    public boolean ge(RedBlackNode that) {
+        if (that == null) {
+            return true;
+        } else {
+            return this.cmp(that) >= 1;
+        }
     }
-    
-    public Boolean le(RedBlackNode that) {
+
+    public boolean le(RedBlackNode that) {
         return this.cmp(that) <= 1;
     }
 
-    public Queue path(RedBlackNode node, Queue path) {
+    public RedBlackQueue path(RedBlackNode node, RedBlackQueue path) {
         // This is the core pathing function.
         // I am the child of the last node in path.
         // I know I'm not the node we're looking for,
@@ -133,9 +148,9 @@ public class RedBlackNode extends Node {
         // Revisit later.
     }
 
-    public Queue path(RedBlackNode node) {
+    public RedBlackQueue path(RedBlackNode node) {
         //A wrapper for the above.
-        Queue path = new Queue();
+        RedBlackQueue path = new RedBlackQueue();
         path.add(this);
         if (this == node) {
             return path;
@@ -143,45 +158,93 @@ public class RedBlackNode extends Node {
         return this.path(node, path);
     }
 
-    public void adopt(RedBlackNode node) {
-            // node's legit. make it mine.
-            // It's on the level below me, if I have a level at all.
-            if (this.level() > -1) {
-                node.setLevel(this.level() + 1);
+    public boolean adopt(RedBlackNode that) {
+        //Attempt to adopt the given node while obeying the invariants of red-black trees.
+        //Return true on success, false on failure.
+        if (this.full()) {
+            return false;
+        }
+        else if (this.red && that.red) {
+            return false;
+        }
+        else if (!that.red) {
+            //Try adopting it. Then check if I'm still in compliance with invariant 4, "Every simple path from a node to a descendant leaf contains the same number of black nodes."
+            //If not, disown the new guy.
+            this.append(that);
+            if(this.obedient()) {
+                return true;
             }
-            // I must keep my children in order if the balancing algorithms are going to work.
-            if(node.ge(this.children[0])) { // The case where I have no other children is accounted for in RedBlackNode.ge
-            this.children[this.numChildren] = node;
-            } else {
-                RedBlackNode temp1 = this.children[0];
-                RedBlackNode temp2 = null;
-                this.children[0] = node;
-                for(int i=1;i<this.numChildren;i++) {
-                    temp2 = this.children[i];
-                    this.children[i] = temp1;
-                    temp1 = temp2;
-                }
+            else {
+                this.disown(that);
+                return false;
             }
-            this.numChildren++;
+        } else {
+            //It's red, I'm not, everything's great.
+            this.append(that);
+            return true;
+        }
     }
 
     public void disown(int i) {
-        this.children[i] = null;
+        this.children[i] = new RedBlackSentinel(0, false, this.tree);
         RedBlackNode temp = null;
-        if(i >= this.numChildren - 1) {
-            this.numChildren--;
-            return;
-        }
-        i++;
-        while(i<this.numChildren) {
+        while (i < this.numChildren) {
             temp = this.children[i];
-            this.children[i-1] = this.children[i];
-            this.children[i] = temp;
+            this.children[i] = this.children[i];
+            this.children[i + 1] = temp;
+            i++;
         }
         this.numChildren--;
     }
+    
+    public void disown(RedBlackNode child) {
+        for(int i=0;i<this.numChildren;i++) {
+            if(this.child(i) == child) {
+                this.disown(i);
+            }
+        }
+    }
 
     public int children() {
-	return this.numChildren;
+        return this.numChildren;
+    }
+
+    public boolean full() {
+        return this.numChildren < RedBlackNode.MAX_CHILDREN;
+    }
+
+    private void append(RedBlackNode node) {
+        //Append the given node to children without checking anything.
+        this.children[this.numChildren] = node;
+        this.numChildren++;
+    }
+    
+    private RedBlackQueue fringe(RedBlackQueue fringe) {
+        for(int i=0;i<this.numChildren;i++) {
+            if(this.child(i).getClass() == RedBlackSentinel.class) {
+                fringe.enqueue(this.child(i));
+            } else {
+                RedBlackQueue lilfringe = this.child(i).fringe(fringe);
+                fringe.addAll(lilfringe);
+            }
+        }
+        return fringe;
+    }
+    
+    public RedBlackQueue fringe() {
+        return this.fringe(new RedBlackQueue());
+    }
+    
+    private boolean obedient() {
+        //Check for compliance with invariant 4.
+        RedBlackQueue fringe = this.fringe();
+        RedBlackNode leaf = fringe.dequeue();
+        int numblack = this.black_in_path(leaf);
+        leaf = fringe.dequeue();
+        while(leaf != null) {
+            if(this.black_in_path(leaf) != numblack) return false;
+            leaf = fringe.dequeue();
+        }
+        return true;
     }
 }
--- src/redblacktree/RedBlackTree.java
+++ src/redblacktree/RedBlackTree.java
@@ -17,71 +17,64 @@ public class RedBlackTree extends Tree {
     RedBlackTree() {
         super();
     }
-    
-    RedBlackTree(RedBlackNode init) {
+
+    RedBlackTree(int init_key) {
         super();
-        this.root = init;
+        this.root = new RedBlackNode(init_key, false, this);
     }
+    
 
-    public int black_per_path() {
-
-    public Boolean empty() {
+    public boolean empty() {
         return this.root == null;
     }
 
-    public Boolean member(RedBlackNode node) {
+    public boolean member(RedBlackNode node) {
         if (this.empty()) {
             return false;
         }
         return this.root.member(node);
     }
-    
+
     public ArrayList<RedBlackNode> path(RedBlackNode node) {
         return this.root.path(node);
     }
 
-    public Boolean insert(RedBlackNode node, RedBlackNode pointer) {
-	//Pointer should be root to begin with
-	//First determine if there's room among pointer's children
-	//where I could put node. If there isn't, recurse into the
-	//child at the would-be insertion point j
-	//--or the last child if j is MAX_CHILDREN--and if the
-	//recursive call turns up false, recurse into the child
-	//just before j, if that wouldn't be redundant.
-	if(pointer.numChildren() == MAX_CHILDREN) {
-	    int j = 0;
-	    while(j < pointer.numChildren()) {
-		if(node.lt(pointer.child(j))) j++;
-		else break;
-	    }
-	    if(j == MAX_CHILDREN) {
-		return this.insert(node, pointer.child(MAX_CHILDREN - 1));
-	    } else {
-		if(this.insert(node, pointer.child(j))) return true;
-		else return this.insert(node, pointer.child(jMAX_CHILDREN));
-	    }
-	} else {
-	    //Since there IS room, check colors.
-	    if(node.red && !pointer.red) {
-		//legit
-		pointer.adopt(node);
-		return true;
-	    } else if(!node.red && pointer.red) {
-		//*potentially* legit, but only if:
-		//
-		//(a) there's at least one other black child already,
-		//which I shall presume was inserted there correctly, or
-		//
-		//(b) the path resulting from insertion is the correct length
-		//
-		//Since all children of a red node must be black, I
-		//don't actually need to check the color--only see if
-		//there are any.
-		if(pointer.numChildren() > 0) {
-		    pointer.adopt(node);
-		    return true;
-		} else if(this.black_per_path() - 1 = 
-		    
-		    
+    public boolean insert(RedBlackNode node, RedBlackNode pointer) {
+        //Pointer should be root to begin with
+        //First determine if there's room among pointer's children
+        //where I could put node. If there isn't, recurse into the
+        //child at the would-be insertion point j
+        //--or the last child if j is MAX_CHILDREN--and if the
+        //recursive call turns up false, recurse into the child
+        //just before j, if that wouldn't be redundant.
+        if (pointer.numChildren() == RedBlackNode.MAX_CHILDREN) {
+            int j = 0;
+            while (j < pointer.numChildren()) {
+                if (node.lt(pointer.child(j))) {
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            if (j == RedBlackNode.MAX_CHILDREN) {
+                return this.insert(node, pointer.child(RedBlackNode.MAX_CHILDREN - 1));
+            } else {
+                if (this.insert(node, pointer.child(j))) {
+                    return true;
+                } else {
+                    return this.insert(node, pointer.child(RedBlackNode.MAX_CHILDREN));
+                }
+            }
+        } else if(pointer.adopt(node)) return true;
+        else {
+            for(int i=0;i<pointer.numChildren();i++) {
+                if(this.insert(node, pointer)) return true;
+            }
+            return false;
+        }
+    }
+    
+    public int black_in_path(RedBlackNode node) {
+        return root.black_in_path(node);
     }
 }
