1c58258fee3d77eecd545b512102e44658010f4a
LogicalDash <LogicalDash@gmail.com>
2012-12-07 13:00:36 -0500

    Added sentinels.

---
+++ src/redblacktree/RedBlackQueue.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redblacktree;
+
+/**
+ *
+ * @author sanotehu
+ */
+public class RedBlackQueue extends Queue {
+
+    	public void enqueue(RedBlackNode element) {
+		if (element != null)
+			add(element);
+	} // end enqueue method
+        
+    @Override
+	public RedBlackNode dequeue() {
+		if (!empty())
+			return (RedBlackNode) remove(0);
+		else
+			return null;
+	} // end dequeue method
+
+    @Override
+	public RedBlackNode front() {
+		if (!empty())
+			return (RedBlackNode) get(0);
+		else
+			return null;
+	} // end front method
+}
---
+++ src/redblacktree/RedBlackSentinel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redblacktree;
+
+/**
+ *
+ * @author sanotehu
+ */
+public class RedBlackSentinel extends RedBlackNode {
+
+    RedBlackSentinel(int key, boolean color, RedBlackTree tree) {
+        super(key, color, tree);
+    }
+}
